f9930dc7f9c57e571f088343717c9d4c8fc251b1
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-01-02 16:20:58 +0530

    feat: enhance sign-in screen with improved styling and add SignIn button

--- app/login/signIn.jsx
+++ app/login/signIn.jsx
@@ -16,17 +16,30 @@ const signin = () => {
           }}>Please sign in to continue</Text>
           <TextInput placeholder="Email" style=
           {{ height: 40,margin:20, borderColor: 'gray', borderWidth: 1, marginBottom: 10,paddingLeft:20,borderRadius:20 ,backgroundColor:'white'}} />
-          <TextInput placeholder="Password" secureTextEntry={true} style={{ height: 40,margin:20, borderColor: 'gray', borderWidth: 1, marginBottom: 10,paddingLeft:20,borderRadius:20 ,backgroundColor:'white'}} />
+          <TextInput placeholder="Password" secureTextEntry={true} style={{ height: 40,marginTop:2, borderColor: 'gray', borderWidth: 1, marginBottom: 10,paddingLeft:20,borderRadius:20 ,backgroundColor:'white',margin:20}} />
           <Text style={{
             borderRadius:20,
             backgroundColor:'gray',
             textAlign:'center',
             color:'white',
-            borderColor:'blue',
+            borderColor:'black',
             margin:35,
             padding:15,
             borderRadius:10,
+            borderWidth:1
           }}  onPress={() => { } } >Login</Text>
+            <Text style={{
+            borderRadius:20,
+            backgroundColor:'white',
+            textAlign:'center',
+            color:'blue',
+            borderColor:'blue',
+            margin:35,
+            marginTop:10,
+            padding:15,
+            borderRadius:10,
+            borderWidth:1
+          }}  onPress={() => { } } >SignIn</Text>
     </View>
   )
 }
